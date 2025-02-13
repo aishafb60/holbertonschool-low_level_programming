060c8c1e78d70967956c3815b26dd6fdd6e822e2
Aisha <basahalaisha@gmail.com>
2025-02-13 19:21:08 +0000

    corrrect

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    return (0);
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
 }
